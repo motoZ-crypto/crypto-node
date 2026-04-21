87918912137d588f69e8ad8faf7df4577b8bb187
motoZ-crypto <motoz.crypto@proton.me>
2026-04-21 11:28:58 +0000

    integrate GRANDPA voter and RPC
    - Add sc-consensus-grandpa-rpc and sc-rpc deps
    - Register GRANDPA notification protocol on the network
    - Expose GRANDPA RPC (grandpa_*) via FullDeps / GrandpaDeps

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -8,23 +8,45 @@
 use std::sync::Arc;
 
 use jsonrpsee::RpcModule;
+use sc_consensus_grandpa::{
+	FinalityProofProvider, GrandpaJustificationStream, SharedAuthoritySet, SharedVoterState,
+};
+use sc_rpc::SubscriptionTaskExecutor;
 use sc_transaction_pool_api::TransactionPool;
 use solochain_template_runtime::{opaque::Block, AccountId, Balance, Nonce};
 use sp_api::ProvideRuntimeApi;
 use sp_block_builder::BlockBuilder;
 use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
+use sp_runtime::traits::Block as BlockT;
+
+/// Dependencies for GRANDPA RPC.
+pub struct GrandpaDeps<B> {
+	/// Voting round info.
+	pub shared_voter_state: SharedVoterState,
+	/// Authority set info.
+	pub shared_authority_set:
+		SharedAuthoritySet<<Block as BlockT>::Hash, <<Block as BlockT>::Header as sp_runtime::traits::Header>::Number>,
+	/// Receives notifications about justification events from GRANDPA.
+	pub justification_stream: GrandpaJustificationStream<Block>,
+	/// Executor to drive the subscription manager in the GRANDPA RPC handler.
+	pub subscription_executor: SubscriptionTaskExecutor,
+	/// Finality proof provider.
+	pub finality_provider: Arc<FinalityProofProvider<B, Block>>,
+}
 
 /// Full client dependencies.
-pub struct FullDeps<C, P> {
+pub struct FullDeps<C, P, B> {
 	/// The client instance to use.
 	pub client: Arc<C>,
 	/// Transaction pool instance.
 	pub pool: Arc<P>,
+	/// GRANDPA specific dependencies.
+	pub grandpa: GrandpaDeps<B>,
 }
 
 /// Instantiate all full RPC extensions.
-pub fn create_full<C, P>(
-	deps: FullDeps<C, P>,
+pub fn create_full<C, P, B>(
+	deps: FullDeps<C, P, B>,
 ) -> Result<RpcModule<()>, Box<dyn std::error::Error + Send + Sync>>
 where
 	C: ProvideRuntimeApi<Block>,
@@ -34,27 +56,34 @@ where
 	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
 	C::Api: BlockBuilder<Block>,
 	P: TransactionPool + 'static,
+	B: sc_client_api::Backend<Block> + Send + Sync + 'static,
 {
 	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
+	use sc_consensus_grandpa_rpc::{Grandpa, GrandpaApiServer};
 	use substrate_frame_rpc_system::{System, SystemApiServer};
 
 	let mut module = RpcModule::new(());
-	let FullDeps { client, pool } = deps;
+	let FullDeps { client, pool, grandpa } = deps;
+	let GrandpaDeps {
+		shared_voter_state,
+		shared_authority_set,
+		justification_stream,
+		subscription_executor,
+		finality_provider,
+	} = grandpa;
 
 	module.merge(System::new(client.clone(), pool).into_rpc())?;
 	module.merge(TransactionPayment::new(client).into_rpc())?;
-
-	// Extend this RPC with a custom API by using the following syntax.
-	// `YourRpcStruct` should have a reference to a client, which is needed
-	// to call into the runtime.
-	// `module.merge(YourRpcTrait::into_rpc(YourRpcStruct::new(ReferenceToClient, ...)))?;`
-
-	// You probably want to enable the `rpc v2 chainSpec` API as well
-	//
-	// let chain_name = chain_spec.name().to_string();
-	// let genesis_hash = client.block_hash(0).ok().flatten().expect("Genesis block exists; qed");
-	// let properties = chain_spec.properties();
-	// module.merge(ChainSpec::new(chain_name, genesis_hash, properties).into_rpc())?;
+	module.merge(
+		Grandpa::new(
+			subscription_executor,
+			shared_authority_set,
+			shared_voter_state,
+			justification_stream,
+			finality_provider,
+		)
+		.into_rpc(),
+	)?;
 
 	Ok(module)
 }
