a383b337220a4025bad438d32da947620b2f33b3
motoZ-crypto <motoz.crypto@proton.me>
2026-04-28 23:38:03 -0700

    add dev mode miner account handling to command and service layers (#64)

--- node/src/command.rs
+++ node/src/command.rs
@@ -173,6 +173,7 @@ pub fn run() -> sc_cli::Result<()> {
 		},
 		None => {
 			let mine = cli.miner;
+			let miner_account = cli.run.get_keyring().unwrap_or(Sr25519Keyring::Alice).to_account_id();
 			let runner = cli.create_runner(&cli.run)?;
 			runner.run_node_until_exit(|config| async move {
 				match config.network.network_backend {
@@ -181,10 +182,10 @@ pub fn run() -> sc_cli::Result<()> {
 							solochain_template_runtime::opaque::Block,
 							<solochain_template_runtime::opaque::Block as sp_runtime::traits::Block>::Hash,
 						>,
-					>(config, mine)
+					>(config, mine, miner_account)
 					.map_err(sc_cli::Error::Service),
 					sc_network::config::NetworkBackendType::Litep2p =>
-						service::new_full::<sc_network::Litep2pNetworkBackend>(config, mine)
+						service::new_full::<sc_network::Litep2pNetworkBackend>(config, mine, miner_account)
 							.map_err(sc_cli::Error::Service),
 				}
 			})
